7fb90574ae9648d26fea6c316282820d5dae9b7b
Bobby Galli <bobbyg603@pm.me>
2025-03-06 16:51:34 -0500

    feat: attributes

--- app/src/main/java/com/bugsplat/android/BugSplatBridge.java
+++ app/src/main/java/com/bugsplat/android/BugSplatBridge.java
@@ -3,6 +3,7 @@ package com.bugsplat.android;
 import android.app.Activity;
 import android.content.pm.ApplicationInfo;
 import android.util.Log;
+import java.util.Map;
 
 public class BugSplatBridge {
     // Static initializer to load native library (equivalent to Kotlin's init block)
@@ -15,15 +16,25 @@ public class BugSplatBridge {
     }
 
     public static void initBugSplat(Activity activity, String database, String application, String version) {
-        initBugSplat(activity, database, application, version, null);
+        initBugSplat(activity, database, application, version, null, null);
     }
 
     public static void initBugSplat(Activity activity, String database, String application, String version,
             String[] attachments) {
+        initBugSplat(activity, database, application, version, null, attachments);
+    }
+
+    public static void initBugSplat(Activity activity, String database, String application, String version,
+            Map<String, String> attributes) {
+        initBugSplat(activity, database, application, version, attributes, null);
+    }
+
+    public static void initBugSplat(Activity activity, String database, String application, String version,
+            Map<String, String> attributes, String[] attachments) {
         ApplicationInfo applicationInfo = activity.getApplicationInfo();
         Log.d("BugSplat", "init result: " +
                 jniInitBugSplat(applicationInfo.dataDir, applicationInfo.nativeLibraryDir, database, application,
-                        version, attachments));
+                        version, attributes, attachments));
     }
 
     public static void crash() {
@@ -31,7 +42,7 @@ public class BugSplatBridge {
     }
 
     static native boolean jniInitBugSplat(String dataDir, String libDir, String database, String application,
-            String version, String[] attachments);
+            String version, Map<String, String> attributes, String[] attachments);
 
     static native void jniCrash();
 }
